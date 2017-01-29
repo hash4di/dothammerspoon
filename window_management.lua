3c6bd31855348dcc7573020567b24eaa785a794e
Marcin Nowicki <pr0d1r2@gmail.com>
2017-01-29 07:10:20 +0100

    Rewrite window management to be DRY

--- window_management.lua
+++ window_management.lua
@@ -1,4 +1,5 @@
 local SCREEN_MODIFIER = {"cmd", "alt", "ctrl"}
+local WINDOW_MODIFIER = {"cmd", "ctrl"}
 
 hs.hotkey.bind(SCREEN_MODIFIER, "Left", function()
   hs.window.focusedWindow():moveOneScreenWest()
@@ -8,38 +9,48 @@ hs.hotkey.bind(SCREEN_MODIFIER, "Right", function()
   hs.window.focusedWindow():moveOneScreenEast()
 end)
 
--- WIP to get rid of window_management_legacy.lua
-local function resizeAndPlaceWindow(x, y, wDiv, hDiv)
+local function resizeAndPlaceWindow(x, y, wDiv, hDiv, wMul, hMul)
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
   local max = screen:frame()
 
-  if type(x) == 'integer' then
+  if type(x) == 'number' then
     f.x = max.x + x
   elseif type(x) == 'string' then -- 50%
-    -- WIP
-    -- f.x = max.x + (max.w * (tonumber(x) / 100))
+    f.x = max.x + (max.w * (tonumber(x) / 100))
   end
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
+  if type(y) == 'number' then
+    f.y = max.y + y
+  elseif type(y) == 'string' then -- 50%
+    f.y = max.y + (max.h * (tonumber(y) / 100))
+  end
+  f.w = max.w / wDiv * wMul
+  f.h = max.h / hDiv * hMul
   win:setFrame(f)
 end
 
--- WIP to get rid of window_management_legacy.lua
--- For testing purposes
-local WINDOW_MODIFIER = {"shift", "cmd", "ctrl"}
--- When finished we will use
---local WINDOW_MODIFIER = {"cmd", "ctrl"}
-local function bindResizeAndPlaceWindow(hkey, x, y, wDiv, hDiv)
+local function bindResizeAndPlaceWindow(hkey, x, y, wDiv, hDiv, wMul, hMul)
   hs.hotkey.bind(WINDOW_MODIFIER, hkey, function()
-    resizeAndPlaceWindow(x, y, wDiv, hDiv)
+    resizeAndPlaceWindow(x, y, wDiv, hDiv, wMul, hMul)
   end)
 end
 
+bindResizeAndPlaceWindow("Return", 0, 0, 1, 1, 1, 1)
 
-bindResizeAndPlaceWindow("Left", 0, 0, 2, 1)
-bindResizeAndPlaceWindow("Right", "50%", 0, 2, 1)
--- bindResizeAndPlaceWindow("Up", 0, 0, 1, 2)
--- bindResizeAndPlaceWindow("Down", 0, "+50%", 2, 1)
+bindResizeAndPlaceWindow("Left", 0, 0, 2, 1, 1, 1)
+bindResizeAndPlaceWindow("Right", "50", 0, 2, 1, 1, 1)
+bindResizeAndPlaceWindow("Up", 0, 0, 1, 2, 1, 1)
+bindResizeAndPlaceWindow("Down", 0, "50", 1, 2, 1, 1)
+
+bindResizeAndPlaceWindow("[", 0, 0, 2, 2, 1, 1)
+bindResizeAndPlaceWindow("]", "50", 0, 2, 2, 1, 1)
+bindResizeAndPlaceWindow("'", 0, "50", 2, 2, 1, 1)
+bindResizeAndPlaceWindow("\\", "50", "50", 2, 2, 1, 1)
+
+bindResizeAndPlaceWindow(",", 0, 0, 3, 1, 1, 1)
+bindResizeAndPlaceWindow(".", "33", 0, 3, 1, 1, 1)
+bindResizeAndPlaceWindow("/", "66", 0, 3, 1, 1, 1)
+
+bindResizeAndPlaceWindow("t", 0, 0, 4, 1, 3, 1)
+bindResizeAndPlaceWindow("y", "25", 0, 4, 1, 3, 1)
