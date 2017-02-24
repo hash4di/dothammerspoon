c38ca4ab62086469a48befc4ff0da2a42544ad9f
Marcin Nowicki <pr0d1r2@gmail.com>
2017-02-24 05:23:31 +0100

    Add OmniFocus rescheduling tool

--- paste_by_typing.lua
+++ paste_by_typing.lua
@@ -2,3 +2,9 @@ hs.hotkey.bind({"cmd", "ctrl", "alt", "shift"}, "v", function()
   text = hs.pasteboard.getContents()
   hs.eventtap.keyStrokes(text)
 end)
+
+-- Useful for rescheduling stuff in OmniFocus
+hs.hotkey.bind({"cmd", "ctrl", "alt", "shift"}, "t", function()
+  hs.eventtap.keyStrokes('today')
+  hs.eventtap.keyStroke({}, "ENTER")
+end)
